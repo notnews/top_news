a3bf64ee40575210dc3b611f3f0457716ec86217
Derek Willis <derek.willis@propublica.org>
2022-06-30 07:11:07 -0400

    added more feed urls

--- npr.py
+++ npr.py
@@ -4,11 +4,13 @@ import feedparser
 with open("npr_urls.json", "r") as npr_file:
     npr_json = json.load(npr_file)
 
-feed = feedparser.parse("https://feeds.npr.org/1001/rss.xml")
+feeds = ["https://feeds.npr.org/1014/rss.xml", "https://feeds.npr.org/1001/rss.xml", "https://feeds.npr.org/1003/rss.xml", "https://feeds.npr.org/1004/rss.xml", "https://feeds.npr.org/1006/rss.xml", "https://feeds.npr.org/1007/rss.xml", "https://feeds.npr.org/1008/rss.xml", "https://feeds.npr.org/1009/rss.xml", "https://feeds.npr.org/1015/rss.xml", "https://feeds.npr.org/1016/rss.xml", "https://feeds.npr.org/1017/rss.xml"]
 
-for article in feed.entries:
-    if not article['link'] in npr_json:
-        npr_json.append(article['link'])
+for url in feeds:
+    feed = feedparser.parse(url)
+    for article in feed.entries:
+        if not article['link'] in npr_json:
+            npr_json.append(article['link'])
 
 with open("npr_urls.json", "w") as f:
     f.write(json.dumps(npr_json))
